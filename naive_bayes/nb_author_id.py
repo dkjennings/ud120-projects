79df9e32a9ef4557fc08610bd3fc506d53a4e01f
Jennings <dkjennings@deloitte.co.uk>
2018-02-20 15:56:11 +0000

    improved function output and comments

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -18,21 +18,31 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+t2 = time()
 features_train, features_test, labels_train, labels_test = preprocess()
-
+print(f"Your preprocessing time was: {round(time()-t2, 3)} s")
 
 #tr -d '\015' <word_data.pkl >word2_data.plk
 
 #########################################################
+#import packages
 import sklearn as sk
 import numpy as np
+#import time
 from sklearn.naive_bayes import GaussianNB
+
+#Initialise functions
+t0 = time()
 clf = GaussianNB()
+
+#Create fit function
 clf.fit(features_train, labels_train)
-#clf_pf = GaussianNB()
-#clf_pf.partial_fit(features_train, labels_train, np.unique(labels_train))
-#testlabels = clf.predict(features_test)
+print(f"Your fitting time was: {round(time()-t0, 3)} s")
+
+#Test against test data
+t1 = time()
 score = clf.score(features_test, labels_test)
+print(f"Your testing time was: {round(time()-t1, 3)} s")
 print(f"the score is: {score:.4f}")
 #########################################################
 
