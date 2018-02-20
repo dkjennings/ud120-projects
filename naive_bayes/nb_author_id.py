771a41ba12e299997123ca0dec28a0e4aa27fdd5
Jennings <dkjennings@deloitte.co.uk>
2018-02-20 15:45:04 +0000

    finished exercise

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,19 +15,25 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+#tr -d '\015' <word_data.pkl >word2_data.plk
 
 #########################################################
-### your code goes here ###
-
-
+import sklearn as sk
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+#clf_pf = GaussianNB()
+#clf_pf.partial_fit(features_train, labels_train, np.unique(labels_train))
+#testlabels = clf.predict(features_test)
+score = clf.score(features_test, labels_test)
+print(f"the score is: {score:.4f}")
 #########################################################
 
 
