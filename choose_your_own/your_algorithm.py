d995118fa546977049eba2b5d956fbb77fc92c55
Jennings <dkjennings@deloitte.co.uk>
2018-03-01 16:18:32 +0000

    Did my own thing :D (Choose your own)

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,7 +3,9 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from time import perf_counter as t
 
+tstart = t()
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
@@ -25,20 +27,59 @@ plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
+
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.ensemble import RandomForestClassifier as rfc
+from sklearn.metrics import accuracy_score as a_s
 
+acctest = 0.8
+N = 5000
+good = 0
+treeid = 0
 
+for i in range(N):
+    tryclf = rfc(criterion='entropy', max_leaf_nodes=10)#, min_samples_split=2)#criterion='entropy', max_leaf_nodes=10, min_samples_split=4
+    tprep = t()
+    tryclf.fit(features_train, labels_train)
+    tfit = t()
+    pred = tryclf.predict(features_test)
+    tpred = t()
+    acc = a_s(labels_test, pred)
+    tacc = t()
 
-
-
-
+    if acc>acctest:
+        print(f"The accuracy of the random forest {i} is {acc*100:.2f}%")
+        #good = input("is this good enough (1=yes/else no)\n\n>>> ")
+        if good == 1:
+            acctest = acc
+            clf = tryclf
+            treeid = i
+            print(f"Tree finished at accuracy {acctest*100:.2f}%")
+        else:
+            treeid = i
+            acctest = acc
+            clf = tryclf
+    if good == 1:
+        break
+    if i == N-1:
+        print(f"Tree finished at accuracy {acctest*100:.2f}%")
 
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
     pass
+
+tpic = t()
+plt.title(f"Random tree classifier id : {treeid}")
+print(f"preparation phase: {tprep-tstart:.2f}s")
+print(f"fitting time is: {tfit-tprep:.2f}s")
+print(f"prediciton time is: {tpred-tfit:.2f}s")
+print(f"Checking time is {tacc-tpred:.2f}s")
+print(f"Picture making time is {tpic-tacc:.2f}s")
+print(f"Total forest time: {tacc-tprep:.2f}s")
+print(f"Total time: {tpic-tstart:.2f}s")
